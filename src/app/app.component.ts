7fa1c9d267f4d989833dc7b9e3afc2b7d19374a7
Philippe Schild <philippe.schild@gmail.com>
2017-03-16 20:12:19 +0100

    added output of filelist in template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {Http} from "@angular/http";
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works!';
+  files = [];
+
+  constructor(private http: Http) {
+    http.get('http://localhost:1234/files/C%3A%5CUsers%5Cjohn%5CPictures')
+        .map(res => res.json())
+        .subscribe(
+          (files) => {
+            this.files = files.files;
+          }
+        );
+  }
 }
