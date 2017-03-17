941b71101ac7ad4db43c117e7eaa89fca4e74d94
Philippe Schild <philippe.schild@gmail.com>
2017-03-17 15:06:24 +0100

    file service can check if a file is a file (or an image file) or a directory

--- server/services/file.service.js
+++ server/services/file.service.js
@@ -12,14 +12,27 @@ FileService.prototype.getFilesByPath = function (givenPath) {
                 reject(error);
             } else {
                 resolve(fileNames.map((fileName) => {
+                    let fullPath = path.join(givenPath, fileName);
+                    let stats = this.getStatOfFile(fullPath);
                     return {
                         fileName: fileName,
-                        fullPath: path.join(givenPath, fileName)
+                        fullPath: fullPath,
+                        isFile: stats.isFile(),
+                        isDirectory: stats.isDirectory(),
+                        isImage: this.isImageFile(fileName)
                     };
                 }));
             }
         });
     });
+};
+
+FileService.prototype.getStatOfFile = function(pathToFile) {
+    return fsp.lstatSync(pathToFile);
+};
+
+FileService.prototype.isImageFile = function(fileName) {
+    return fileName.match(/\.(jpg|jpeg|png|gif)$/i) != null;
 };
 
 exports = module.exports = new FileService();
