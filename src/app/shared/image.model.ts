6455a311dac716ade498228f74705d702de4aeae
Philippe Schild <philippe.schild@googlemail.com>
2017-03-28 11:10:52 +0200

    added properties for image model

--- src/app/shared/image.model.ts
+++ src/app/shared/image.model.ts
@@ -2,4 +2,11 @@ export interface Image {
     id: number;
     name: string;
     path: string;
+    comment: string;
+    fromDay: number;
+    fromMonth: number;
+    fromYear: number;
+    toDay: number;
+    toMonth: number;
+    toYear: number;
 }
