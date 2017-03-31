bf54ce83d51eb600c13f2156e5b69674201e5660
Philippe Schild <philippe.schild@googlemail.com>
2017-03-31 19:57:20 +0200

    logic for creating or updating multiple images in the editor

--- src/app/image-form/image-form.component.ts
+++ src/app/image-form/image-form.component.ts
@@ -1,9 +1,8 @@
 import {Component, Input, OnDestroy, OnInit} from '@angular/core';
-import {Image} from "../shared/image.model";
 import {Store} from "@ngrx/store";
 import {AppState} from "../shared/reducers";
 import {createImage, updateImage} from "../editor/editor.actions";
-import {Subscription} from "rxjs";
+import {Observable, Subscription} from "rxjs";
 import {ImageService} from "../image/image.service";
 import {ImageGetResponse} from "../shared/responses";
 
@@ -40,18 +39,60 @@ export class ImageFormComponent implements OnInit, OnDestroy {
     ngOnInit() {
         this.formModel = this.initialValues;
 
-        let item = this.items[0];
-        this.subscriptions.push(this.imageService.getImage(encodeURI(item.path), item.fileName)
-            .subscribe((response: ImageGetResponse) => {
-                this.images = [response.image || {}]; // push empty object so that Object.assign can be used when saving
-                if (response.image) {
-                    this.formModel = Object.assign(this.initialValues, response.image);
-                } else {
-                    // set path and fileName when no image is found in the database
-                    this.formModel = Object.assign(this.initialValues, { path: encodeURI(item.path), name: item.fileName });
+        if (this.items.length === 1) {
+            let item = this.items[0];
+            this.subscriptions.push(this.imageService.getImage(encodeURI(item.path), item.fileName)
+                .subscribe((response: ImageGetResponse) => {
+                    this.images = [response.image || {}]; // push empty object so that Object.assign can be used when saving
+                    if (response.image) {
+                        this.formModel = Object.assign(this.initialValues, response.image);
+                    } else {
+                        // set path and fileName when no image is found in the database
+                        this.formModel = Object.assign(this.initialValues, {
+                            path: encodeURI(item.path),
+                            name: item.fileName
+                        });
+                    }
+                    this.isTimePeriod = this.checkForTimePeriod();
+                }));
+
+        } else if (this.items.length > 1) {
+            let observables = [];
+            this.items.forEach((item) => {
+                observables.push(this.imageService.getImage(encodeURI(item.path), item.fileName));
+            });
+
+            this.subscriptions.push(Observable.forkJoin(observables)
+                .subscribe((responses: ImageGetResponse[]) => {
+                    this.images = responses.filter(response => response.image).map((response: ImageGetResponse) => {
+                        return response.image;
+                    });
+                    this.formModel = Object.assign(this.initialValues, this.getCommonValues());
+                    this.isTimePeriod = this.checkForTimePeriod();
+                }));
+        }
+    }
+
+    getCommonValues() {
+        let commonValues = {};
+        Object.keys(this.initialValues).forEach((key) => {
+            let firstValue = this.images[0][key];
+            let differenceFound = false;
+
+            for (let i = 1; i < this.images.length; i++) {
+                let image = this.images[i];
+                if (image[key] !== firstValue) {
+                    differenceFound = true;
+                    break;
                 }
-                this.isTimePeriod = this.checkForTimePeriod();
-            }));
+            }
+
+            if (!differenceFound) {
+                commonValues[key] = firstValue;
+            }
+        });
+
+        return commonValues;
     }
 
     checkForTimePeriod() {
@@ -65,11 +106,12 @@ export class ImageFormComponent implements OnInit, OnDestroy {
     }
 
     onSubmit() {
-        this.images.forEach((image: Image) => {
-            if (image.id) {
-                this.store.dispatch(updateImage(Object.assign(image, this.formModel)));
+        this.items.forEach((item: any) => {
+            let relatedImage = this.images.filter(image => image.path === item.path && image.name === item.fileName)[0];
+            if (relatedImage) {
+                this.store.dispatch(updateImage(Object.assign(relatedImage, this.formModel)));
             } else {
-                this.store.dispatch(createImage(Object.assign(image, this.formModel)));
+                this.store.dispatch(createImage(Object.assign(item, this.formModel, { name: item.fileName })));
             }
         });
     }
