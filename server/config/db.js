27da3921425054d8e875cf575a24c17f5727eabd
pschild <philippe.schild@gmail.com>
2017-05-16 19:45:22 +0200

    tables are now created dynamically

--- server/config/db.js
+++ server/config/db.js
@@ -10,4 +10,33 @@ var knex = require('knex')({
 });
 var bookshelf = require('bookshelf')(knex);
 
+bookshelf.knex.schema.createTableIfNotExists('image', function(table) {
+    table.increments();
+    table.string('name');
+    table.string('path');
+    table.text('comment');
+    table.integer('fromDay');
+    table.integer('fromMonth');
+    table.integer('fromYear');
+    table.integer('toDay');
+    table.integer('toMonth');
+    table.integer('toYear');
+}).then(function() {
+    console.log('table created successfully.');
+});
+
+bookshelf.knex.schema.createTableIfNotExists('tag', function(table) {
+    table.increments();
+    table.string('name');
+}).then(function() {
+    console.log('table created successfully.');
+});
+
+bookshelf.knex.schema.createTableIfNotExists('image_tag', function(table) {
+    table.integer('image_id');
+    table.integer('tag_id');
+}).then(function() {
+    console.log('table created successfully.');
+});
+
 module.exports.bookshelf = bookshelf;
